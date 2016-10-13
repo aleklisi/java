bb9911da2824bb9c84fc61fb6eb220a4b377f685
aleklisi <sebaczechu123@gmail.com>
2016-10-13 17:08:24 +0200

    Add files via upload

---
+++ cw2str90.java
@@ -0,0 +1,10 @@
+
+/*
+ * @autor Aleksander Lisicecki
+ * @version 1.0 i ostatnia :p
+ */
+public class cw2str90 {
+	public static void main(String [] args){
+		System.out.print("Witaj Swiecie ");
+	}
+}
